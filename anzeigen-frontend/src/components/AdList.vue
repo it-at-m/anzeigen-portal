39d165c8ffe9b8a28fc195f16407ef99bbd51c30
jannik.lange <jannik.lange@muenchen.de>
2024-10-08 15:51:26 +0200

    created small demo components #25

---
+++ anzeigen-frontend/src/components/AdList.vue
@@ -0,0 +1,9 @@
+<template>
+  <sheet> hello world</sheet>
+</template>
+
+<script setup lang="ts">
+import Sheet from "@/components/common/Sheet.vue";
+</script>
+
+<style scoped></style>
---
+++ anzeigen-frontend/src/components/AdSearch.vue
@@ -0,0 +1,9 @@
+<template>
+  <sheet> Das ist in eine sheet und hoffentlich weiss </sheet>
+</template>
+
+<script setup lang="ts">
+import Sheet from "@/components/common/Sheet.vue";
+</script>
+
+<style scoped></style>
