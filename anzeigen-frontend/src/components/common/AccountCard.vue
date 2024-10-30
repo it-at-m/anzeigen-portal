9a76e8e75954e2e95f1264ed8e06bfd9f4a9e3ca
jannik.lange <jannik.lange@muenchen.de>
2024-10-30 14:37:44 +0100

    add account card #31

---
+++ anzeigen-frontend/src/components/common/AccountCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card
+    flat
+    class="mb-4"
+    title="Vorname Nachname"
+    subtitle="Hier gehts zu Ihren Anzeigen"
+    link
+  >
+    <template #prepend>
+      <ad2-image-avatar username="vorname nachname" />
+    </template>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import Ad2ImageAvatar from "@/components/common/Ad2ImageAvatar.vue";
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
+</script>
+
+<style scoped></style>
