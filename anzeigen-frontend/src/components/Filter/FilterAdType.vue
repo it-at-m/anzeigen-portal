963e943b0faebee325296af81a89037665a8cde1
jannik.lange <jannik.lange@muenchen.de>
2024-10-31 09:23:27 +0100

    added url logic for filtering ad type #31

--- anzeigen-frontend/src/components/Filter/FilterAdType.vue
+++ anzeigen-frontend/src/components/Filter/FilterAdType.vue
@@ -3,14 +3,14 @@
     <template #title>Nach Art filtern</template>
     <template #text>
       <v-checkbox
-        v-model="isOffer"
+        v-model="isSeek"
         label="Suche"
         hide-details
         density="compact"
         color="accent"
       />
       <v-checkbox
-        v-model="isSeek"
+        v-model="isOffer"
         label="Biete"
         hide-details
         density="compact"
@@ -21,20 +21,46 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { useUrlSearchParams } from "@vueuse/core";
+import { onMounted, ref, watch } from "vue";
 
 import AdDisplayCard from "@/components/common/AdDisplayCard.vue";
+import { ROUTER_TYPE } from "@/Constants";
 
 const isOffer = ref<boolean>(true);
 const isSeek = ref<boolean>(true);
 
+const params = useUrlSearchParams(ROUTER_TYPE);
+
+/**
+ * Initializes checkbox selections based on the "type" URL parameter.
+ * Sets either "Offer" or "Seek" as deselected if the URL specifies a type.
+ */
+onMounted(() => {
+  if (params.type === "OFFER") {
+    isSeek.value = false;
+  } else if (params.type === "SEEK") {
+    isOffer.value = false;
+  }
+});
+
+/**
+ * Watches changes in the checkbox selections and adjusts selections and URL parameter accordingly.
+ * Ensures at least one option (Offer or Seek) remains selected at all times.
+ * If both options are selected, the URL parameter is cleared.
+ */
 watch([isOffer, isSeek], ([newIsOffer, newIsSeek], [oldIsOffer, oldIsSeek]) => {
   if (!newIsOffer && !oldIsSeek) {
     isSeek.value = true;
   } else if (!newIsSeek && !oldIsOffer) {
     isOffer.value = true;
   }
+
+  // Update URL parameter
+  if (isOffer.value && isSeek.value) {
+    params.type = [];
+  } else {
+    params.type = isOffer.value ? "OFFER" : "SEEK";
+  }
 });
 </script>
-
-<style scoped></style>
