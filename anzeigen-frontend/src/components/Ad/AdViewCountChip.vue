54de070fbb9e548332354603cb964ec66d4d3df0
jannik.lange <jannik.lange@muenchen.de>
2024-11-06 13:51:36 +0100

    reworked chips - added props #31

---
+++ anzeigen-frontend/src/components/Ad/AdViewCountChip.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-chip
+    variant="tonal"
+    :prepend-icon="computedIcon"
+    color="accent"
+    :text="views.toString()"
+  />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const { views = 0 } = defineProps<{
+  views: number;
+}>();
+
+const computedIcon = computed(() =>
+  views <= 0 ? "mdi-eye-off-outline" : "mdi-eye-outline"
+);
+</script>
+
+<style scoped></style>
