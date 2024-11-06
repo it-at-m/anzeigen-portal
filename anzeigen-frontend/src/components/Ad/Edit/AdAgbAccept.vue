56b8cf248681236725a890cb4fb8839e3968b8fd
jannik.lange <jannik.lange@muenchen.de>
2024-11-06 13:55:46 +0100

    added agb component #31

---
+++ anzeigen-frontend/src/components/Ad/Edit/AdAgbAccept.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-checkbox
+    v-model="isChecked"
+    color="accent"
+  >
+    <template #label>
+      <p>
+        Ich bin mit den <a>Nutzungsbedingungen (AGB)</a> einverstanden. Das
+        Schwarze Brett darf nur zu rein privaten Zwecken genutzt werden.
+        Insbesondere Werbung kommerzieller Art ist nicht gestattet!
+        Informationen zur Verarbeitung personenbezogener Daten enthalten die
+        <a>Datenschutzhinweise</a>.
+      </p>
+    </template>
+  </v-checkbox>
+</template>
+
+<script setup lang="ts">
+const isChecked = defineModel<boolean>();
+</script>
+
+<style scoped></style>
