b8a1daac6f934ba0093c4a906b698e448dc720e6
jannik.lange <jannik.lange@muenchen.de>
2024-11-07 12:13:44 +0100

    pmd - cyclomaticcomplexity

--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/ads/service/AdValidationService.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/ads/service/AdValidationService.java
@@ -15,30 +15,54 @@ public class AdValidationService {
     private SettingService settingService;
 
     public void validate(Ad ad) {
+        validateImage(ad);
 
+        validateFiles(ad);
+    }
+
+    private void validateImage(Ad ad) {
         if (ad.getImageOriginal() != null && ad.getImageOriginal().hasImage()) {
-            Integer maxImgSize = settingService.getSetting(SettingName.MAX_SWB_IMAGE_SIZE).getNumberValue();
-            Integer maxImgSizeBytes = maxImgSize * 1024 * 1024;
+            int maxImgSizeBytes = getMaxImageSizeInBytes();
             if (ad.getImageOriginal().getImageLength() > maxImgSizeBytes) {
-                throw new ValidationException("Validierung fehlgeschlagen: Foto darf maximal " + maxImgSize + " MB groß sein!");
+                throw new ValidationException("Validierung fehlgeschlagen: Foto darf maximal " + (maxImgSizeBytes / (1024 * 1024)) + " MB groß sein!");
             }
         }
+    }
 
-        if (ad.getFiles().size() > 0) {
-            Integer maxFileSize = settingService.getSetting(SettingName.MAX_SWB_FILE_SIZE).getNumberValue();
-            Integer maxFilesLength = settingService.getSetting(SettingName.MAX_SWB_FILES_LENGTH).getNumberValue();
+    private void validateFiles(Ad ad) {
+        if (!ad.getFiles().isEmpty()) {
+            int maxFilesLength = getMaxFilesLength();
 
-            if (ad.getFiles().size() > (maxFilesLength)) {
+            if (ad.getFiles().size() > maxFilesLength) {
                 throw new ValidationException("Validierung fehlgeschlagen: Maximal " + maxFilesLength + " Dateien erlaubt!");
             }
 
             for (SwbFile file : ad.getFiles()) {
-                Integer maxFileSizeBytes = maxFileSize * 1024 * 1024;
-                if (file.hasFile() && file.getFileLength() > maxFileSizeBytes) {
-                    throw new ValidationException("Validierung fehlgeschlagen: Datei darf maximal " + maxFileSize + " MB groß sein!");
-                }
+                validateFile(file);
             }
         }
+    }
+
+    private void validateFile(SwbFile file) {
+        int maxFileSizeBytes = getMaxFileSizeInBytes();
+
+        if (file.hasFile() && file.getFileLength() > maxFileSizeBytes) {
+            throw new ValidationException("Validierung fehlgeschlagen: Datei darf maximal " + (maxFileSizeBytes / (1024 * 1024)) + " MB groß sein!");
+        }
+    }
+
+    private int getMaxImageSizeInBytes() {
+        Integer maxImgSize = settingService.getSetting(SettingName.MAX_SWB_IMAGE_SIZE).getNumberValue();
+        return maxImgSize * 1024 * 1024;
+    }
+
+    private int getMaxFileSizeInBytes() {
+        Integer maxFileSize = settingService.getSetting(SettingName.MAX_SWB_FILE_SIZE).getNumberValue();
+        return maxFileSize * 1024 * 1024;
+    }
+
+    private int getMaxFilesLength() {
+        return settingService.getSetting(SettingName.MAX_SWB_FILES_LENGTH).getNumberValue();
     }
 
 }
