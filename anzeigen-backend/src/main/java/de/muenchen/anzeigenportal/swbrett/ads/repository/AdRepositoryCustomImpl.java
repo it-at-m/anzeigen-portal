9161da568353298345a6387371abd8244e7eb1a5
jannik.lange <jannik.lange@muenchen.de>
2024-11-07 12:01:44 +0100

    pmd - CouplingBetweenObjects

--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/ads/repository/AdRepositoryCustomImpl.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/ads/repository/AdRepositoryCustomImpl.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("PMD.CouplingBetweenObjects")
 @Repository
 public class AdRepositoryCustomImpl implements AdRepositoryCustom {
 
