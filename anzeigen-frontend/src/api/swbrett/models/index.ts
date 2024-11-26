6d6d8efe0128dd228632a65dab451bc934e8b459
jannik.lange <jannik.lange@muenchen.de>
2024-11-26 15:30:29 +0100

    :alien: generated api code

---
+++ anzeigen-frontend/src/api/swbrett/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdCategory';
+export * from './AdTO';
+export * from './SettingTO';
+export * from './SwbFileTO';
+export * from './SwbImageTO';
+export * from './SwbUserTO';
