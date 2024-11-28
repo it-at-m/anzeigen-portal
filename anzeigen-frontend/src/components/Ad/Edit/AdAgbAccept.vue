ef90e1ffb5194832f79e04a8d77acf174a127ab9
jannik.lange <jannik.lange@muenchen.de>
2024-11-28 14:56:55 +0100

    Added downloadable file for agb accept component

--- anzeigen-frontend/src/components/Ad/Edit/AdAgbAccept.vue
+++ anzeigen-frontend/src/components/Ad/Edit/AdAgbAccept.vue
@@ -5,18 +5,51 @@
   >
     <template #label>
       <p>
-        Ich bin mit den <a>Nutzungsbedingungen (AGB)</a> einverstanden. Das
+        Ich bin mit den
+        <a :href="agbFile">Nutzungsbedingungen (AGB)</a> einverstanden. Das
         Schwarze Brett darf nur zu rein privaten Zwecken genutzt werden.
         Insbesondere Werbung kommerzieller Art ist nicht gestattet!
         Informationen zur Verarbeitung personenbezogener Daten enthalten die
-        <a>Datenschutzhinweise</a>.
+        <a :href="securityFile">Datenschutzhinweise</a>.
       </p>
     </template>
   </v-checkbox>
 </template>
 
 <script setup lang="ts">
-const isChecked = defineModel<boolean>();
-</script>
+import { useObjectUrl } from "@vueuse/core";
+import { onMounted, ref } from "vue";
 
-<style scoped></style>
+import AGB from "@/static/AGB-1.pdf";
+import SecInfo from "@/static/Datenschutzhinweise.pdf";
+
+/**
+ * Value if checkbox is checked
+ */
+const isChecked = defineModel<boolean>({ default: false });
+
+/**
+ * Blob and objectUrl for AGB
+ */
+const agbBlob = ref();
+const agbFile = useObjectUrl(agbBlob);
+
+/**
+ * Blob and objectUrl for Datenschutzhinweise
+ */
+const securityBlob = ref();
+const securityFile = useObjectUrl(securityBlob);
+
+/**
+ * Load files on mount
+ */
+onMounted(async () => {
+  const [agbResponse, secResponse] = await Promise.all([
+    fetch(AGB),
+    fetch(SecInfo),
+  ]);
+
+  agbBlob.value = await agbResponse.blob();
+  securityBlob.value = await secResponse.blob();
+});
+</script>
