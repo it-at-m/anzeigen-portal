69dd3f38992d0a0ab20f5e6487708237e336a4da
jannik.lange <jannik.lange@muenchen.de>
2024-11-28 15:02:03 +0100

    Added constant for maximum title length

--- anzeigen-frontend/src/Constants.ts
+++ anzeigen-frontend/src/Constants.ts
@@ -11,3 +11,8 @@ export const ROUTER_TYPE = "hash";
  * EventBus Identifiers
  */
 export const EV_EDIT_AD_DIALOG = "eventbus-dialog";
+
+/**
+ * Other constants
+ */
+export const AD_MAX_TITLE_LENGTH = 40;
