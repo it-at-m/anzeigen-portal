817240553f6e170f47c14f09527b2a6c882a6b67
jannik.lange <jannik.lange@muenchen.de>
2024-12-05 16:12:18 +0100

    regenerated api files

--- anzeigen-frontend/src/api/swbrett/models/index.ts
+++ anzeigen-frontend/src/api/swbrett/models/index.ts
@@ -1,6 +1,8 @@
-export * from "./AdCategory";
-export * from "./AdTO";
-export * from "./SettingTO";
-export * from "./SwbFileTO";
-export * from "./SwbImageTO";
-export * from "./SwbUserTO";
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdCategory';
+export * from './AdTO';
+export * from './SettingTO';
+export * from './SwbFileTO';
+export * from './SwbImageTO';
+export * from './SwbUserTO';
