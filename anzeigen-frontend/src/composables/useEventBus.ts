5dc977df76c50139c3f70485ef69299045896170
jannik.lange <jannik.lange@muenchen.de>
2025-01-07 15:04:16 +0100

    added eventbus for updating adList

--- anzeigen-frontend/src/composables/useEventBus.ts
+++ anzeigen-frontend/src/composables/useEventBus.ts
@@ -1,5 +1,7 @@
 import { useEventBus } from "@vueuse/core";
 
-import { EV_CLEAR_CACHE } from "@/Constants";
+import { EV_CLEAR_CACHE, EV_UPDATE_AD_LIST } from "@/Constants";
 
 export const useClearCacheEventBus = () => useEventBus(EV_CLEAR_CACHE);
+
+export const useUpdateAdListEventBus = () => useEventBus(EV_UPDATE_AD_LIST);
