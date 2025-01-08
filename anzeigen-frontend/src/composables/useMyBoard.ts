3b0da6864613e785d0b69d3523e65d206eb2a890
jannik.lange <jannik.lange@muenchen.de>
2025-01-08 09:26:45 +0100

    removed composable

--- anzeigen-frontend/src/composables/useMyBoard.ts
+++
@@ -1,9 +0,0 @@
-import { computed } from "vue";
-import { useRoute } from "vue-router";
-
-const useMyBoard = () => {
-  const route = useRoute();
-  return computed(() => route.path === "/myboard");
-};
-
-export { useMyBoard };
