24b11564ce6de7b41650bba798f15f027d279782
jannik.lange <jannik.lange@muenchen.de>
2025-01-08 17:22:13 +0100

    adapted icon text to display a link

--- anzeigen-frontend/src/components/common/IconText.vue
+++ anzeigen-frontend/src/components/common/IconText.vue
@@ -6,16 +6,26 @@
       :icon="'mdi-' + icon"
       class="mr-1"
     />
-    <a :href="href">{{ label }}</a>
+    <a
+      :href="href"
+      :class="{ 'link-text': link, 'text-decoration-underline': link }"
+    >
+      {{ label }}
+    </a>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+const { link = false } = defineProps<{
+  link?: boolean;
   label: string;
   href?: string;
   icon?: string;
 }>();
 </script>
 
-<style scoped></style>
+<style scoped>
+.link-text {
+  color: #0066d3;
+}
+</style>
