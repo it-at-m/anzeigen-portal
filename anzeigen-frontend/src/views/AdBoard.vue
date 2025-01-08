42493b6cf6c904fa6e9415c70896d6ad658ddfd2
jannik.lange <jannik.lange@muenchen.de>
2025-01-08 17:22:31 +0100

    removed prop isMyboard

--- anzeigen-frontend/src/views/AdBoard.vue
+++ anzeigen-frontend/src/views/AdBoard.vue
@@ -12,34 +12,24 @@
         lg="9"
         cols="12"
       >
-        <ad-list :is-my-board="isMyBoard" />
+        <ad-list />
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, provide, ref, toRef } from "vue";
+import { onMounted, ref } from "vue";
 
 import { checkHealth } from "@/api/health-client";
 import AdDialog from "@/components/Ad/Edit/AdDialog.vue";
 import AdList from "@/components/AdList.vue";
 import AdNavBar from "@/components/AdNavBar.vue";
 import { useSnackbar } from "@/composables/useSnackbar";
-import { IK_IS_MYBOARD } from "@/Constants";
 import HealthState from "@/types/HealthState";
 
 const snackbar = useSnackbar();
 const status = ref("DOWN");
-
-const { isMyBoard = false } = defineProps<{
-  isMyBoard: boolean;
-}>();
-
-provide(
-  IK_IS_MYBOARD,
-  toRef(() => isMyBoard)
-);
 
 onMounted(() => {
   checkHealth()
