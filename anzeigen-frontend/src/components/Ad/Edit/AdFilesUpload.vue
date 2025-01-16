7fd6e925bea0e08e8a1a7dc16f4d8d86229d4193
jannik.lange <jannik.lange@muenchen.de>
2025-01-16 17:08:10 +0100

    fixed uploading of files. Now works with backend

--- anzeigen-frontend/src/components/Ad/Edit/AdFilesUpload.vue
+++ anzeigen-frontend/src/components/Ad/Edit/AdFilesUpload.vue
@@ -1,20 +1,30 @@
 <template>
-  <v-file-upload
-    :model-value="computedFiles"
-    clearable
-    density="compact"
-    multiple
-    divider-text="Das ist der Dividertext"
-    show-size
-    @update:model-value="uploadedFile"
-  />
+  <div class="w-md-66 w-sm-75 my-2">
+    <v-file-upload
+      :model-value="computedFiles"
+      clearable
+      density="compact"
+      multiple
+      title="Optionale Anhänge hochladen"
+      show-size
+      @update:model-value="uploadedFile"
+    >
+      <template #item="{ file }">
+        <v-file-upload-item
+          density="compact"
+          :file="file"
+          @click:remove="removeFile(file)"
+        />
+      </template>
+    </v-file-upload>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { SwbFileTO } from "@/api/swbrett";
 
 import { computed } from "vue";
-import { VFileUpload } from "vuetify/labs/VFileUpload";
+import { VFileUpload, VFileUploadItem } from "vuetify/labs/VFileUpload";
 
 const { modelValue } = defineProps<{
   modelValue?: SwbFileTO[];
@@ -30,30 +40,29 @@ const computedFiles = computed(() => {
   }
 
   return modelValue.map((swbFile) => {
-    const data = window.atob(swbFile.fileBase64 || "");
-    const blob = new Blob([data]);
-    return new File([blob], swbFile.name || "undefined", {
-      type: "application/octet-stream",
-    });
+    // fuck the backend - imageBase64 will always be 0 length
+    return new File(
+      [new Uint8Array(swbFile.size || 0)],
+      swbFile.name || "undefined",
+      {
+        type: "application/octet-stream",
+      }
+    );
   });
 });
 
-//const computedFiles = () => {
-//  if (!modelValue || modelValue.length === 0) {
-//    return [];
-//  }
-//
-//  return modelValue.map((swbFile) => {
-//    // const data = window.atob(swbFile.fileBase64 || "");
-//    const blob = new Blob([]);
-//    return new File([blob], swbFile.name || "undefined", {
-//      type: "application/octet-stream",
-//    });
-//  });
-//};
+const removeFile = (removeFile: File) => {
+  if (!modelValue) {
+    return;
+  }
+
+  emit(
+    "update:modelValue",
+    modelValue.filter((file) => file.name !== removeFile.name)
+  );
+};
 
 const uploadedFile = async (uploads: File[]) => {
-  console.log("Called upload File before", uploads.length);
   const toReturn: SwbFileTO[] = modelValue || [];
 
   for (const file of uploads) {
@@ -64,12 +73,19 @@ const uploadedFile = async (uploads: File[]) => {
     ) {
       continue;
     }
-    const base64 = window.btoa(await file.text());
-    toReturn.push({
-      fileBase64: base64,
-      size: file.size,
-      name: file.name,
-    });
+
+    const reader = new FileReader();
+    reader.onloadend = function () {
+      const result = (reader.result as string).split(",")[1];
+
+      toReturn.push({
+        size: file.size,
+        name: file.name,
+        fileBase64: window.btoa(result),
+      });
+    };
+
+    reader.readAsDataURL(file);
   }
 
   emit("update:modelValue", toReturn);
--- anzeigen-frontend/src/components/Ad/Edit/AdTitelPicture.vue
+++ anzeigen-frontend/src/components/Ad/Edit/AdTitelPicture.vue
@@ -1,41 +1,94 @@
 <template>
-  <v-file-upload
-    v-if="!computedPicture"
-    clearable
-    density="compact"
-    browse-text="Titelbild hochladen"
-    title="Titelbild hochladen"
-    accept="image/png"
-    reverse
+  <div class="w-md-66 w-sm-75">
+    <v-file-upload
+      v-if="!computedPicture"
+      title="Optionales Titelbild hochladen"
+      density="compact"
+      @update:model-value="uploadPicture"
+    />
+    <v-file-upload-item
+      v-else
+      title="Ein Titelbild wurde ausgewählt."
+      clearable
+      @click:remove="clearFile"
+    >
+      <template #prepend>
+        <v-icon icon="mdi-panorama-outline" />
+      </template>
+    </v-file-upload-item>
+  </div>
+  <v-file-input
+    v-if="false"
+    :model-value="computedPicture"
+    prepend-icon=""
+    label="Titelbild"
     @update:model-value="uploadPicture"
-  />
-  <v-file-upload-item
-    v-else
-    :file="computedPicture"
-    clearable
-    size
-    @click:remove="clearFile"
-  />
+  >
+    <template #prepend>
+      <v-icon
+        v-if="!computedPicture"
+        icon="mdi-panorama-outline"
+      />
+      <img
+        v-else
+        :src="computedPreview"
+        alt="Hallo"
+        height="50px"
+      />
+    </template>
+  </v-file-input>
 </template>
 
 <script setup lang="ts">
 import type { SwbImageTO } from "@/api/swbrett";
 
 import { computed } from "vue";
+import { VFileUpload, VFileUploadItem } from "vuetify/labs/VFileUpload";
 
-import AdFilesUpload from "@/components/Ad/Edit/AdFilesUpload.vue";
+const { modelValue } = defineProps<{
+  modelValue: SwbImageTO | undefined;
+}>();
 
-defineProps<{
-  modelValue: SwbImageTO;
+const emit = defineEmits<{
+  "update:modelValue": [modelValue: SwbImageTO | undefined];
 }>();
 
 const computedPicture = computed(() => {
-  return true;
+  if (!modelValue || (!modelValue.id && !modelValue.imageBase64)) {
+    return undefined;
+  }
+
+  return new File([new Blob([""])], "Titelbild", {
+    type: "image/jpeg",
+  });
 });
 
-const uploadPicture = (file: File[]) => {};
+const computedPreview = computed(
+  () => "data:image/jpeg;base64," + modelValue?.imageBase64
+);
 
-const clearFile = () => {};
+const uploadPicture = async (files: File[] | File) => {
+  const file = Array.isArray(files) ? files[0] : files;
+
+  if (!file) {
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.onloadend = function () {
+    const result = (reader.result as string).split(",")[1];
+    const base64 = window.btoa(result);
+    emit("update:modelValue", {
+      imageBase64: base64,
+    });
+  };
+
+  reader.readAsDataURL(file);
+};
+
+const clearFile = () => {
+  emit("update:modelValue", undefined);
+};
 </script>
 
 <style scoped></style>
