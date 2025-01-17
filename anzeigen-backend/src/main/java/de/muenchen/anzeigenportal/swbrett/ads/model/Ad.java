43c04773119cf17203cd37ab59cb8d8902a3e1c2
jannik.lange <jannik.lange@muenchen.de>
2025-01-17 15:44:17 +0100

    :lock: disallowed editing of ads from all users

--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/ads/model/Ad.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/ads/model/Ad.java
@@ -7,7 +7,6 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
