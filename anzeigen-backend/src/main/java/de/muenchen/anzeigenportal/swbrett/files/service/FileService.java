6075b586600de0906f8de440a5e1290920296dba
jannik.lange <jannik.lange@muenchen.de>
2025-01-21 10:03:23 +0100

    save

--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/files/service/FileService.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/files/service/FileService.java
@@ -4,7 +4,9 @@ import de.muenchen.anzeigenportal.swbrett.files.model.SwbFile;
 import de.muenchen.anzeigenportal.swbrett.files.model.SwbFileTO;
 import de.muenchen.anzeigenportal.swbrett.files.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class FileService {
@@ -16,7 +18,7 @@ public class FileService {
     private FileMapper mapper;
 
     public SwbFileTO getFileTO(final long id) {
-        final SwbFile file = repository.getOne(id);
+        final SwbFile file = repository.findBy(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "File not found"));;
         return mapper.toSwbFileTO(file);
     }
 
