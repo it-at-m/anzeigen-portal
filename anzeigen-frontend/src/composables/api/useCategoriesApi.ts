bbf88b652583b3c05ecad4a2f9d768267cb0b429
jannik.lange <jannik.lange@muenchen.de>
2025-01-28 15:17:01 +0100

    added api requests

--- anzeigen-frontend/src/composables/api/useCategoriesApi.ts
+++ anzeigen-frontend/src/composables/api/useCategoriesApi.ts
@@ -1,11 +1,41 @@
-import type { AdCategory } from "@/api/swbrett";
+import type {
+  AdCategory,
+  CreateAdCategoryRequest,
+  DeleteAdCategoryRequest,
+  UpdateAdCategoryRequest,
+} from "@/api/swbrett";
 
 import { useApiCall } from "@/composables/api/useApiCall";
 import { ApiFactory } from "@/util/apiFactory.ts";
 
-export const useCategoriesApi = () => {
+export const useGetCategories = () => {
   const api = ApiFactory.getInstance();
 
   // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
   return useApiCall<void, AdCategory[]>(() => api.getAllAdCategories());
+};
+
+export const useDeleteAdCategory = () => {
+  const api = ApiFactory.getInstance();
+
+  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+  return useApiCall<DeleteAdCategoryRequest, void>((params) =>
+    api.deleteAdCategory(params)
+  );
+};
+
+export const useCreateAdCategory = () => {
+  const api = ApiFactory.getInstance();
+
+  return useApiCall<CreateAdCategoryRequest, AdCategory>((params) =>
+    api.createAdCategory(params)
+  );
+};
+
+export const useUpdateAdCategory = () => {
+  const api = ApiFactory.getInstance();
+
+  return useApiCall<UpdateAdCategoryRequest, AdCategory>((params) =>
+    api.updateAdCategory(params)
+  );
 };
