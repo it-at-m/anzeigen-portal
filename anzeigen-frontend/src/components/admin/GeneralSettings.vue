b5587e467617acec72188844d3220e1a542b0ea7
jannik.lange <jannik.lange@muenchen.de>
2025-01-29 15:55:05 +0100

    added first layout for generalSettings

--- anzeigen-frontend/src/components/admin/GeneralSettings.vue
+++ anzeigen-frontend/src/components/admin/GeneralSettings.vue
@@ -2,61 +2,81 @@
   <ad-display-card>
     <template #title>Allgemeine Einstellungen</template>
     <template #text>
-      <v-text-field
-        v-for="setting in settingStore.settings"
-        :key="setting.id"
-        :label="setting.settingName"
-        :model-value="
-          setting.numberValue ||
-          setting.textValue ||
-          setting.flagValue ||
-          setting.fileValue
-        "
-        variant="outlined"
-        color="accent"
-      />
+      <text-select-value-setting />
+      <v-row>
+        <v-col>
+          <text-value-setting setting-name="MOTD" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <text-value-setting setting-name="DEFAULT_ORDERING" />
+        </v-col>
+        <v-col
+          cols="12"
+          md="6"
+        >
+          <text-value-setting setting-name="DEFAULT_SORTING" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          v-for="settingName in NUMBER_VALUE_SETTINGS"
+          :key="settingName"
+          cols="12"
+          md="6"
+          lg="4"
+        >
+          <number-value-setting :setting-name="settingName" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="d-flex justify-end">
+          <v-btn
+            class="mr-2"
+            variant="outlined"
+            prepend-icon="mdi-undo-variant"
+            color="accent"
+            @click="updateSettings"
+          >
+            Änderungen verwerfen
+          </v-btn>
+          <v-btn
+            variant="flat"
+            prepend-icon="mdi-content-save"
+            color="accent"
+          >
+            Speichern
+          </v-btn>
+        </v-col>
+      </v-row>
     </template>
   </ad-display-card>
 </template>
 
 <script setup lang="ts">
-import type { SettingTO } from "@/api/swbrett";
+import type { SettingTOSettingNameEnum } from "@/api/swbrett";
 
-import { onMounted } from "vue";
-
-import { Levels } from "@/api/error.ts";
+import NumberValueSetting from "@/components/admin/general/NumberValueSetting.vue";
+import TextSelectValueSetting from "@/components/admin/general/TextSelectValueSetting.vue";
+import TextValueSetting from "@/components/admin/general/TextValueSetting.vue";
 import AdDisplayCard from "@/components/common/AdDisplayCard.vue";
-import { useGetSettings } from "@/composables/api/useSettingsApi.ts";
-import { useSnackbar } from "@/composables/useSnackbar.ts";
-import { API_ERROR_MSG } from "@/Constants.ts";
-import { useSettingStore } from "@/stores/settings.ts";
+import { useUpdateSettings } from "@/composables/useUpdateSettings.ts";
+import { CRITERIA_VALUES } from "../../types/SortingOrderCriteria.ts";
 
-const settingStore = useSettingStore();
+const updateSettings = useUpdateSettings();
 
-const snackbar = useSnackbar();
-
-const {
-  call: getSettings,
-  data: settingsData,
-  loading: settingsLoading,
-  error: settingsError,
-} = useGetSettings();
-
-onMounted(async () => {
-  if (!settingStore.isLoaded) {
-    await getSettings();
-
-    if (settingsError.value) {
-      snackbar.sendMessage({
-        level: Levels.ERROR,
-        message: API_ERROR_MSG,
-      });
-      return;
-    }
-
-    settingStore.setSettings(settingsData.value as SettingTO[]);
-  }
-});
+const NUMBER_VALUE_SETTINGS: SettingTOSettingNameEnum[] = [
+  "MAX_SWB_IMAGE_SIZE",
+  "MAX_PAGE_SIZE",
+  "MAX_SWB_FILE_SIZE",
+  "MAX_SWB_FILES_LENGTH",
+  "MAX_EXPIRY_DATE_RANGE",
+  "MAX_ARCHIVE_DATE_RANGE",
+];
 
 /**
  * Loads settings and stores them in the settings store.
