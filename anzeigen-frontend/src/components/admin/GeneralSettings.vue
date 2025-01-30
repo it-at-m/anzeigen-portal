0ad6880622aade03ac18667b77087d47e97c2d84
jannik.lange <jannik.lange@muenchen.de>
2025-01-30 13:36:53 +0100

    added all settings to the general settings overview

--- anzeigen-frontend/src/components/admin/GeneralSettings.vue
+++ anzeigen-frontend/src/components/admin/GeneralSettings.vue
@@ -2,7 +2,6 @@
   <ad-display-card>
     <template #title>Allgemeine Einstellungen</template>
     <template #text>
-      <text-select-value-setting />
       <v-row>
         <v-col>
           <text-value-setting setting-name="MOTD" />
@@ -13,15 +12,16 @@
           cols="12"
           md="6"
         >
-          <text-value-setting setting-name="DEFAULT_ORDERING" />
+          <text-select-value-setting setting-name="DEFAULT_SORTING" />
         </v-col>
         <v-col
           cols="12"
           md="6"
         >
-          <text-value-setting setting-name="DEFAULT_SORTING" />
+          <text-select-value-setting setting-name="DEFAULT_ORDERING" />
         </v-col>
       </v-row>
+
       <v-row>
         <v-col
           v-for="settingName in NUMBER_VALUE_SETTINGS"
@@ -34,20 +34,30 @@
         </v-col>
       </v-row>
       <v-row>
+        <v-col>
+          <file-value-setting setting-name="AGB_FILE" />
+          <file-value-setting setting-name="DATENSCHUTZHINWEISE_FILE" />
+        </v-col>
+      </v-row>
+      <v-row>
         <v-col class="d-flex justify-end">
           <v-btn
             class="mr-2"
             variant="outlined"
             prepend-icon="mdi-undo-variant"
+            :disabled="!settingStore.isDirty || updateSettingsLoading"
             color="accent"
-            @click="updateSettings"
+            @click="reloadSettings"
           >
             Änderungen verwerfen
           </v-btn>
           <v-btn
             variant="flat"
+            :disabled="!settingStore.isDirty"
             prepend-icon="mdi-content-save"
             color="accent"
+            :loading="updateSettingsLoading"
+            @click="saveSettings"
           >
             Speichern
           </v-btn>
@@ -58,16 +68,31 @@
 </template>
 
 <script setup lang="ts">
-import type { SettingTOSettingNameEnum } from "@/api/swbrett";
+import type { SettingTO, SettingTOSettingNameEnum } from "@/api/swbrett";
 
+import { Levels } from "@/api/error.ts";
+import FileValueSetting from "@/components/admin/general/FileValueSetting.vue";
 import NumberValueSetting from "@/components/admin/general/NumberValueSetting.vue";
 import TextSelectValueSetting from "@/components/admin/general/TextSelectValueSetting.vue";
 import TextValueSetting from "@/components/admin/general/TextValueSetting.vue";
 import AdDisplayCard from "@/components/common/AdDisplayCard.vue";
+import { usePutSettings } from "@/composables/api/useSettingsApi.ts";
+import { useSnackbar } from "@/composables/useSnackbar.ts";
 import { useUpdateSettings } from "@/composables/useUpdateSettings.ts";
-import { CRITERIA_VALUES } from "../../types/SortingOrderCriteria.ts";
+import { useSettingStore } from "@/stores/settings.ts";
 
-const updateSettings = useUpdateSettings();
+const reloadSettings = useUpdateSettings();
+
+const settingStore = useSettingStore();
+
+const snackbar = useSnackbar();
+
+const {
+  call: updateSettings,
+  loading: updateSettingsLoading,
+  data: updateSettingsData,
+  error: updateSettingsError,
+} = usePutSettings();
 
 const NUMBER_VALUE_SETTINGS: SettingTOSettingNameEnum[] = [
   "MAX_SWB_IMAGE_SIZE",
@@ -78,10 +103,25 @@ const NUMBER_VALUE_SETTINGS: SettingTOSettingNameEnum[] = [
   "MAX_ARCHIVE_DATE_RANGE",
 ];
 
-/**
- * Loads settings and stores them in the settings store.
- */
-const loadSettings = async () => {};
+const saveSettings = async () => {
+  await updateSettings({
+    settingTO: settingStore.settings,
+  });
+
+  if (updateSettingsError.value) {
+    snackbar.sendMessage({
+      level: Levels.WARNING,
+      message: "Fehler beim Speichern der Einstellungen.",
+    });
+  } else {
+    snackbar.sendMessage({
+      level: Levels.SUCCESS,
+      message: "Einstellungen wurden erfolgreich gespeichert.",
+    });
+  }
+
+  settingStore.setSettings(updateSettingsData.value as SettingTO[]);
+};
 </script>
 
 <style scoped></style>
