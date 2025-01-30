244f1ceb6a0bb00c5bef0837b3b15cbba3d2a24f
jannik.lange <jannik.lange@muenchen.de>
2025-01-30 13:34:04 +0100

    :beers: added components for changing configs

---
+++ anzeigen-frontend/src/components/admin/general/NumberValueSetting.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-number-input
+    variant="outlined"
+    hide-details="auto"
+    density="compact"
+    :model-value="computedSetting?.numberValue"
+    color="accent"
+    :label="settingName"
+    @update:model-value="updatedValue"
+  />
+</template>
+
+<script setup lang="ts">
+import type { SettingTOSettingNameEnum } from "@/api/swbrett";
+
+import { computed } from "vue";
+import { VNumberInput } from "vuetify/labs/components";
+
+import { useSettingStore } from "@/stores/settings.ts";
+
+const settingStore = useSettingStore();
+
+const { settingName } = defineProps<{
+  settingName: SettingTOSettingNameEnum;
+}>();
+
+const computedSetting = computed(() => settingStore.getSetting(settingName));
+
+const updatedValue = (value: number) => {
+  settingStore.setSetting({ ...computedSetting.value, numberValue: value });
+};
+</script>
+
+<style scoped></style>
---
+++ anzeigen-frontend/src/components/admin/general/TextSelectValueSetting.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-select
+    :model-value="computedSetting?.textValue"
+    :label="settingName"
+    hide-details
+    variant="outlined"
+    density="compact"
+    :items="options"
+    @update:model-value="updatedValue"
+  />
+</template>
+
+<script setup lang="ts">
+import type { SettingTOSettingNameEnum } from "@/api/swbrett";
+
+import { computed } from "vue";
+
+import { useSettingStore } from "@/stores/settings.ts";
+
+const CRITERIA_VALUES = ["title", "price", "creationDateTime"];
+const SORTING_VALUES = ["asc", "desc"];
+
+const settingStore = useSettingStore();
+
+const { settingName } = defineProps<{
+  settingName: SettingTOSettingNameEnum;
+}>();
+
+const options = computed(() =>
+  settingName === "DEFAULT_ORDERING" ? SORTING_VALUES : CRITERIA_VALUES
+);
+
+const computedSetting = computed(() => settingStore.getSetting(settingName));
+
+const updatedValue = (value: string) => {
+  settingStore.setSetting({ ...computedSetting.value, textValue: value });
+};
+</script>
+
+<style scoped></style>
