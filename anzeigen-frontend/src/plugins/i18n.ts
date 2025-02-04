7b8f6e92755d9c2106821b4d1aaabfb84c4bbecb
jannik.lange <jannik.lange@muenchen.de>
2025-02-04 16:38:03 +0100

    added first example translations

--- anzeigen-frontend/src/plugins/i18n.ts
+++ anzeigen-frontend/src/plugins/i18n.ts
@@ -1,5 +1,26 @@
 import { createI18n } from "vue-i18n";
 
-export default createI18n({
-  // something vue-i18n options here ...
+import swbrett from "@/locales/swbrett.json";
+
+export const supportedLocales = ["swbrett", "gebrauchtwarenboerse"];
+
+export type SupportedLocale = (typeof supportedLocales)[number];
+
+type MessageSchema = typeof swbrett;
+
+export const defaultLocale: SupportedLocale = "swbrett";
+
+const i18n = createI18n<[MessageSchema], SupportedLocale>({
+  locale: defaultLocale,
+  fallbackLocale: defaultLocale,
+  messages: {
+    swbrett: Object.assign(swbrett),
+  },
 });
+export default i18n;
+
+const setI18nLanguage = (locale: SupportedLocale) => {
+  // any typecast needed because typescript does not know composition mode of i18n is used
+  (i18n.global.locale as any).value = locale;
+  document.querySelector("html")?.setAttribute("lang", locale);
+};
