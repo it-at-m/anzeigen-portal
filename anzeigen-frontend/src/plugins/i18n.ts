ee26646b8005e3e126ec7b43a7bb0f57cfdc9d39
jannik.lange <jannik.lange@muenchen.de>
2025-02-19 10:10:57 +0100

    added dynamic changeable language support

--- anzeigen-frontend/src/plugins/i18n.ts
+++ anzeigen-frontend/src/plugins/i18n.ts
@@ -1,3 +1,4 @@
+import { nextTick } from "vue";
 import { createI18n } from "vue-i18n";
 
 import de from "@/locales/de.json";
@@ -10,6 +11,8 @@ type MessageSchema = typeof de;
 
 export const defaultLocale: SupportedLocale = "de";
 
+const loadedLanguages: SupportedLocale[] = [defaultLocale];
+
 const i18n = createI18n<[MessageSchema], SupportedLocale>({
   locale: defaultLocale,
   fallbackLocale: defaultLocale,
@@ -21,6 +24,34 @@ export default i18n;
 
 const setI18nLanguage = (locale: SupportedLocale) => {
   // any typecast needed because typescript does not know composition mode of i18n is used
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (i18n.global.locale as any).value = locale;
   document.querySelector("html")?.setAttribute("lang", locale);
+};
+
+/**
+ * load another language on demand - language needs to be registered
+ * @param locale one of the registered languages
+ */
+export const loadLanguage = async (locale: SupportedLocale) => {
+  if (!supportedLocales.includes(locale)) {
+    throw new Error("Language not supported");
+  }
+
+  // any typecast needed because typescript does not know composition mode of i18n is used
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if ((i18n.global.locale as any).value === locale) {
+    return nextTick();
+  }
+
+  if (loadedLanguages.includes(locale)) {
+    setI18nLanguage(locale);
+    return nextTick();
+  }
+
+  const messages = await import(`@/locales/${locale}.json`);
+  i18n.global.setLocaleMessage<MessageSchema>(locale, messages.default);
+  setI18nLanguage(locale);
+
+  return nextTick();
 };
