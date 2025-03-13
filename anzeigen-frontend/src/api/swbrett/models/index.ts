20be821dafed1cdf4f243a43bf6de9e2e773b431
jannik.lange <jannik.lange@muenchen.de>
2025-03-13 15:31:12 +0100

    regenerated api

---
+++ anzeigen-frontend/src/api/swbrett/models/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdCategory';
+export * from './AdTO';
+export * from './GetAds200Response';
+export * from './GetAds200ResponseSort';
+export * from './SettingTO';
+export * from './SwbFileTO';
+export * from './SwbImageSanitize';
+export * from './SwbImageTO';
+export * from './SwbUserFind';
+export * from './SwbUserTO';
