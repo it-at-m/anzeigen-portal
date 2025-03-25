c34bdeca233c1649cc61b215898e774052ff3ede
jannik.lange <jannik.lange@muenchen.de>
2025-03-25 13:56:51 +0100

    introduced general setting wrapper with new ordering

---
+++ anzeigen-frontend/src/components/admin/general/SettingWithDescription.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-row>
+    <v-col
+      cols="12"
+      md="4"
+      align-self="center"
+    >
+      <v-tooltip
+        location="top"
+        :text="t(`applicationSettings.${settingName}.tooltip`)"
+      >
+        <template #activator="{ props }">
+          <p v-bind="props">
+            {{ t(`applicationSettings.${settingName}.description`) }}
+          </p>
+        </template>
+      </v-tooltip>
+    </v-col>
+    <v-col
+      cols="12"
+      md="8"
+    >
+      <slot :setting-name="settingName" />
+    </v-col>
+  </v-row>
+  <v-divider class="my-2" />
+</template>
+
+<script setup lang="ts">
+import type { SettingTOSettingNameEnum } from "@/api/swbrett";
+
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+
+defineProps<{
+  settingName: SettingTOSettingNameEnum;
+}>();
+</script>
+
+<style scoped></style>
