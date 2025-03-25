300756c0e17070e41db7610866153fc56a3bedaa
jannik.lange <jannik.lange@muenchen.de>
2025-03-25 13:39:24 +0100

    added new filter for admins

---
+++ anzeigen-frontend/src/components/filter/FilterAdActive.vue
@@ -0,0 +1,32 @@
+<template>
+  <ad-display-card>
+    <template #title>Inaktive Anzeigen</template>
+    <template #text>
+      <v-checkbox
+        v-model="activeAds"
+        label="Aktive Anzeigen"
+        hide-details
+        density="compact"
+        color="accent"
+      />
+    </template>
+  </ad-display-card>
+</template>
+
+<script setup lang="ts">
+import { useRouteQuery } from "@vueuse/router";
+import { ref, watch } from "vue";
+
+import AdDisplayCard from "@/components/common/AdDisplayCard.vue";
+import { QUERY_IS_ACTIVE } from "@/Constants.ts";
+
+const activeAds = ref<boolean>(true);
+
+const isActiveQuery = useRouteQuery(QUERY_IS_ACTIVE);
+
+watch(activeAds, (newActiveAds) => {
+  isActiveQuery.value = newActiveAds.toString();
+});
+</script>
+
+<style scoped></style>
