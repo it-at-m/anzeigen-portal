986f89253daffa558347d3e8f6cdb6924449f83d
jannik.lange <jannik.lange@muenchen.de>
2025-04-23 13:34:39 +0200

    extended service functionality

--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/subscription/repository/SubscriptionRepository.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/subscription/repository/SubscriptionRepository.java
@@ -3,7 +3,9 @@ package de.muenchen.anzeigenportal.swbrett.subscription.repository;
 import de.muenchen.anzeigenportal.swbrett.subscription.model.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
@@ -12,6 +14,11 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, UUID
 
     List<Subscription> findByAdCategory_Id(Long adCategory_id);
 
+    Optional<Subscription> findBySwbUser_IdAndAdCategory_Id(Long swbUser_id, Long adCategory_id);
+
     boolean deleteBySwbUser_IdAndAdCategory_Id(Long swbUser_id, Long adCategory_id);
 
+    boolean existsBySwbUser_IdAndAdCategory_Id(Long swbUser_id, Long adCategory_id);
+
+    void deleteBySubscriptionDateAfter(LocalDate subscriptionDate);
 }
--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/subscription/service/SubscriptionService.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/subscription/service/SubscriptionService.java
@@ -1,15 +1,18 @@
 package de.muenchen.anzeigenportal.swbrett.subscription.service;
 
-import de.muenchen.anzeigenportal.swbrett.ads.model.AdCategory;
 import de.muenchen.anzeigenportal.swbrett.ads.service.AdCategoryService;
 import de.muenchen.anzeigenportal.swbrett.subscription.model.Subscription;
 import de.muenchen.anzeigenportal.swbrett.subscription.repository.SubscriptionRepository;
+import de.muenchen.anzeigenportal.swbrett.users.model.SwbUser;
 import de.muenchen.anzeigenportal.swbrett.users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SubscriptionService {
@@ -20,6 +23,9 @@ public class SubscriptionService {
 
     private final AdCategoryService adCategoryService;
 
+    static private final int MAXIMUM_EXPIRY_DAYS_SUBSCRIPTION = 30;
+    static private final int SOON_EXPIRY_REMINDER_DAYS = 3;
+
     @Autowired
     public SubscriptionService(SubscriptionRepository subscriptionRepository, UserService userService, AdCategoryService adCategoryService) {
         this.subscriptionRepository = subscriptionRepository;
@@ -27,13 +33,22 @@ public class SubscriptionService {
         this.adCategoryService = adCategoryService;
     }
 
-    public void createSubscription(Long categoryId) {
+    public Subscription createSubscription(Long categoryId) {
+        final SwbUser currentUser = userService.getCurrentUser();
+
+        final Optional<Subscription> existingSubscription = subscriptionRepository
+                .findBySwbUser_IdAndAdCategory_Id(currentUser.getId(), categoryId);
+
+        if (existingSubscription.isPresent()) {
+            return existingSubscription.get();
+        }
+
         Subscription subscription = new Subscription();
-        subscription.setSubscriptionDate(new Date());
-        subscription.setSwbUser(userService.getCurrentUser());
+        subscription.setSubscriptionDate(LocalDate.now());
+        subscription.setSwbUser(currentUser);
         subscription.setAdCategory(adCategoryService.getAdCategory(categoryId));
 
-        subscriptionRepository.save(subscription);
+        return subscriptionRepository.save(subscription);
 
         // TODO: send E-Mail Notification?
     }
@@ -41,12 +56,38 @@ public class SubscriptionService {
     public void deleteSubscription(Long subscriptionId) {
         final boolean isDeleted = subscriptionRepository.deleteBySwbUser_IdAndAdCategory_Id(userService.getCurrentUser().getId(), subscriptionId);
 
-        // TODO - fail silently anyway?
+        // TODO - fail silently anyway? - yes
     }
 
     public List<Subscription> getAllSubscriptionOfCategory(Long categoryId) {
         return subscriptionRepository.findByAdCategory_Id(categoryId);
     }
+
+    public Subscription getUserSubscriptionForCategory(Long categoryId) {
+        SwbUser currentUser = userService.getCurrentUser();
+        return subscriptionRepository.findBySwbUser_IdAndAdCategory_Id(currentUser.getId(), categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    public List<Subscription> getAllSubscriptionOfUser() {
+        final SwbUser currentUser = userService.getCurrentUser();
+        return subscriptionRepository.findBySwbUser_Id(currentUser.getId());
+    }
+
+    // TODO - used later
+    public List<Subscription> getAllSoonExpiringSubscripions() {
+        // thought prozess: this will get called daily so we do not want to remind every subscriber daily after time threshold
+        // to mitigate this issue we only get the subscriptions of a specific date
+
+        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    // TODO - used later
+    public void deleteExpiredSubscriptions() {
+        final LocalDate expiryDate = LocalDate.now().plusDays(MAXIMUM_EXPIRY_DAYS_SUBSCRIPTION);
+        subscriptionRepository.deleteBySubscriptionDateAfter(expiryDate);
+    }
+
 
 
 
