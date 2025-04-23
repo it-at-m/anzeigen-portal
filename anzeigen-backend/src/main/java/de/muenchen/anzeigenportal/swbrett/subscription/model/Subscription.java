be40268a6a54f3ed4199bdc1cccf6c4bdd055e39
jannik.lange <jannik.lange@muenchen.de>
2025-04-23 13:34:13 +0200

    changed table name and changed deprecated date to localdate

--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/subscription/model/Subscription.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/subscription/model/Subscription.java
@@ -3,29 +3,24 @@ package de.muenchen.anzeigenportal.swbrett.subscription.model;
 import de.muenchen.anzeigenportal.domain.BaseEntity;
 import de.muenchen.anzeigenportal.swbrett.ads.model.AdCategory;
 import de.muenchen.anzeigenportal.swbrett.users.model.SwbUser;
-import jakarta.persistence.Column;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@Entity(name = "t_subscription")
 public class Subscription extends BaseEntity {
 
     @Column(name = "subscriptionDate")
-    private Date subscriptionDate;
+    private LocalDate subscriptionDate;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "fk_swb_user_id", nullable = false)
     private SwbUser swbUser;
 
-    @Setter
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "fk_ad_category_id", nullable = false)
     private AdCategory adCategory;
