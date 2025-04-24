efda0714bd3d961f8878935a6a23f2eb3cc51049
jannik.lange <jannik.lange@muenchen.de>
2025-04-24 14:56:59 +0200

    removed unnesessary contructor and pmd issues

--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/subscription/controller/SubscriptionController.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/subscription/controller/SubscriptionController.java
@@ -1,9 +1,8 @@
 package de.muenchen.anzeigenportal.swbrett.subscription.controller;
 
-import de.muenchen.anzeigenportal.swbrett.subscription.model.Subscription;
 import de.muenchen.anzeigenportal.swbrett.subscription.model.SubscriptionTO;
 import de.muenchen.anzeigenportal.swbrett.subscription.service.SubscriptionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,14 +10,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/subscriptions")
+@RequiredArgsConstructor
 public class SubscriptionController {
 
-    SubscriptionService subscriptionService;
-
-    @Autowired
-    public SubscriptionController(SubscriptionService subscriptionService) {
-        this.subscriptionService = subscriptionService;
-    }
+    private final SubscriptionService subscriptionService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -28,21 +23,20 @@ public class SubscriptionController {
 
     @GetMapping("/{categoryId}")
     @ResponseStatus(HttpStatus.OK)
-    public SubscriptionTO isUserSubscribedToCategory(@PathVariable long categoryId) {
+    public SubscriptionTO getUserSubscribedToCategory(@PathVariable final Long categoryId) {
         return subscriptionService.getUserSubscriptionForCategory(categoryId);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public SubscriptionTO createUserSubscription(@RequestBody final long categoryId) {
+    public SubscriptionTO createUserSubscription(@RequestBody final Long categoryId) {
         return subscriptionService.createSubscription(categoryId);
     }
 
     @DeleteMapping("/{categoryId}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteUserSubscription(@PathVariable final long categoryId) {
+    public void deleteUserSubscription(@PathVariable final Long categoryId) {
         subscriptionService.deleteSubscription(categoryId);
     }
-
 
 }
