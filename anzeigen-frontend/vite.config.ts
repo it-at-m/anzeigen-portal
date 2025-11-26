ad74c6aae3c7cd18278729419eb66e4d9249911e
langehm <38280183+langehm@users.noreply.github.com>
2025-11-26 10:01:27 +0100

    :hammer: added vite multi-build and both dev servers

--- anzeigen-frontend/vite.config.ts
+++ anzeigen-frontend/vite.config.ts
@@ -3,52 +3,67 @@ import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
 import ViteFonts from "unplugin-fonts/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vueDevTools from "vite-plugin-vue-devtools";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
+import { ImportMetaEnv } from "./env";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-      features: {
-        optionsAPI: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "") as unknown as ImportMetaEnv;
+  const variant = env.VITE_APP_VARIANT || (mode as "swb" | "gbr");
+
+  return {
+    base: env.VITE_BASE_PATH || `/${variant}/`,
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+        features: {
+          optionsAPI: false,
+        },
+      }),
+      vuetify(),
+      vueDevTools(),
+      ViteFonts({
+        google: {
+          families: [
+            {
+              name: "Roboto",
+              styles: "wght@100;300;400;500;700;900",
+            },
+          ],
+        },
+      }),
+    ],
+    server: {
+      host: true,
+      port: 8081,
+      proxy: {
+        "/api": "http://localhost:8083",
+        "/actuator": "http://localhost:8083",
       },
-    }),
-    vuetify(),
-    vueDevTools(),
-    ViteFonts({
-      google: {
-        families: [
-          {
-            name: "Roboto",
-            styles: "wght@100;300;400;500;700;900",
-          },
-        ],
+      allowedHosts: ["host.docker.internal"], // required to use frontend behind proxy (e.g. API Gateway)
+      headers: {
+        "x-frame-options": "SAMEORIGIN", // required to use devtools behind proxy (e.g. API Gateway)
       },
-    }),
-  ],
-  server: {
-    host: true,
-    port: 8081,
-    proxy: {
-      "/api": "http://localhost:8083",
-      "/actuator": "http://localhost:8083",
     },
-    allowedHosts: ["host.docker.internal"], // required to use frontend behind proxy (e.g. API Gateway)
-    headers: {
-      "x-frame-options": "SAMEORIGIN", // required to use devtools behind proxy (e.g. API Gateway)
+    resolve: {
+      alias: {
+        "@variants": fileURLToPath(
+          new URL(`./src/variants/${variant}`, import.meta.url)
+        ),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
     },
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    build: {
+      minify: true,
+      outDir: `dist/${variant}`,
+      emptyOutDir: false,
     },
-    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-  },
-  build: {
-    minify: true,
-  },
-  define: { "process.env": {} },
+    define: {
+      "process.env": {},
+    },
+  };
 });
