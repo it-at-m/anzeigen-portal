66721d2b537830d6b7d822b01123cd88290961e2
langehm <38280183+langehm@users.noreply.github.com>
2025-11-26 10:05:16 +0100

    :sparkles: added example implementation for variant switching

---
+++ anzeigen-frontend/src/variants/swb/components/service-avatar.vue
@@ -0,0 +1,18 @@
+<template>
+  <a
+    href="https://wilma.muenchen.de/pages/gesamtpersonalrat"
+    target="_blank"
+  >
+    <v-toolbar-title class="font-weight-bold d-flex justify-end">
+      <v-avatar
+        size="36px"
+        :image="emblem"
+      />
+    </v-toolbar-title>
+  </a>
+</template>
+
+<script setup lang="ts">
+import emblem from "@/assets/GPR-Logo.jpg";
+import ServiceAvatar from "@/variants/gbr/components/service-avatar.vue";
+</script>
