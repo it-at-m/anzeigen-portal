f6dca2fd7ba2af518fd0ce38f550b39a9a78931e
langehm <38280183+langehm@users.noreply.github.com>
2026-01-08 11:01:22 +0100

    :white_check_mark: updated vitest config

--- anzeigen-frontend/vitest.config.ts
+++ anzeigen-frontend/vitest.config.ts
@@ -1,28 +1,31 @@
-import { fileURLToPath } from "node:url";
+import { fileURLToPath, URL } from "node:url";
 
 import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
 
 import viteConfig from "./vite.config";
 
-// Wert für mode bestimmen; 'test' oder eine Ihrer Varianten
-const baseMode = process.env.VITE_APP_VARIANT ?? "test";
+const variant = process.env.VITE_APP_VARIANT === "gbr" ? "gbr" : "swb";
 
 const resolvedViteConfig =
-  typeof viteConfig === "function"
-    ? viteConfig({ mode: baseMode })
-    : viteConfig;
+  typeof viteConfig === "function" ? viteConfig({ mode: variant }) : viteConfig;
 
 export default mergeConfig(
   resolvedViteConfig,
   defineConfig({
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@variants": fileURLToPath(
+          new URL(`./src/variants/${variant}`, import.meta.url)
+        ),
+      },
+    },
     test: {
       globals: true,
       environment: "jsdom",
       exclude: [...configDefaults.exclude, "e2e/*"],
       root: fileURLToPath(new URL("./", import.meta.url)),
-      server: {
-        deps: { inline: ["vuetify"] },
-      },
+      server: { deps: { inline: ["vuetify"] } },
     },
   })
 );
