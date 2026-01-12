e63d1b8757fa2ae484a2dedc65b3a9481842165a
langehm <38280183+langehm@users.noreply.github.com>
2026-01-12 14:56:56 +0100

    :sparkles: added version display to admin menu

--- anzeigen-frontend/src/views/AdminBoard.vue
+++ anzeigen-frontend/src/views/AdminBoard.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <edit-categories class="my-4" />
-    <general-settings />
+    <general-settings class="my-4" />
+    <version-display />
   </div>
 </template>
 
 <script setup lang="ts">
 import EditCategories from "@/components/admin/EditCategories.vue";
 import GeneralSettings from "@/components/admin/GeneralSettings.vue";
+import VersionDisplay from "@/components/admin/VersionDisplay.vue";
 </script>
 
 <style scoped></style>
