2976fd214b9373e18f549b432152c2f0f5e86738
langehm <38280183+langehm@users.noreply.github.com>
2026-01-12 14:34:17 +0100

    :sparkles: added version display for frontend and backend

---
+++ anzeigen-frontend/src/components/admin/VersionDisplay.vue
@@ -0,0 +1,36 @@
+<template>
+  <ad-display-card>
+    <template #title> {{ t(`versionDisplay.heading`) }} </template>
+    <template #text>
+      <v-row>
+        <v-col>
+          <ad-display-card>
+            <template #subtitle> {{ t(`versionDisplay.backend`) }} </template>
+            <template #text>{{ appInfoData?.application.version }}</template>
+          </ad-display-card>
+        </v-col>
+        <v-col>
+          <ad-display-card>
+            <template #subtitle> {{ t(`versionDisplay.frontend`) }} </template>
+            <template #text>{{ version }}</template>
+          </ad-display-card>
+        </v-col>
+      </v-row>
+    </template>
+  </ad-display-card>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useI18n } from "vue-i18n";
+
+import AdDisplayCard from "@/components/common/AdDisplayCard.vue";
+import { useGetAppInfo } from "@/composables/api/useSettingsApi.ts";
+
+const { t } = useI18n();
+
+const version = import.meta.env.VITE_APP_VERSION;
+const { data: appInfoData, call: appInfoCall } = useGetAppInfo();
+
+onMounted(() => appInfoCall());
+</script>
