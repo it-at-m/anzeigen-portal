68bd9621925a45e3cefade973f92cc5f26b9bc7e
langehm <38280183+langehm@users.noreply.github.com>
2026-03-30 12:08:25 +0200

    :bug: added transactional to method call

--- anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/job/service/ScheduledJobService.java
+++ anzeigen-backend/src/main/java/de/muenchen/anzeigenportal/swbrett/job/service/ScheduledJobService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -26,6 +27,7 @@ public class ScheduledJobService {
     private AdRepository adRepository;
 
     @Scheduled(cron = "${scheduled.deactivateAds}")
+    @Transactional
     public void deactivateExpiredAds() {
 
         final LocalDate limit;
